32a72be42ac5957c7d2e45d94a79481bdbb348e2
azzip <azzipstar@gmail.com>
2022-02-23 11:58:56 +0800

    first

---
+++ Assets/Scripts/MouseClickMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClickMove : MonoBehaviour
+{
+
+    [SerializeField] private GameObject model;
+    [SerializeField] private float speed = 1;
+    private bool moveState = false;
+    private Vector3 target = new Vector3();
+
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0) && hit.transform.gameObject.tag == "floor")
+            {
+                moveState = true;
+                target = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            }
+        }
+        float step = speed * Time.deltaTime;
+        if (moveState)
+        {
+            if (Vector3.Distance(model.transform.position, target) < 0.01f)
+            {
+                moveState = false;
+            }
+            model.transform.position = Vector3.MoveTowards(model.transform.position, target, step);
+        }
+    }
+}
